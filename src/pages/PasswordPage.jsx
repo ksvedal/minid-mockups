a93bec2cd25eeaa3ce2e449f192784ade709b1c1
AudunOklevik <audun.oklevik@gmail.com>
2023-06-27 10:03:13 +0200

    Added placeholders, renamed files and added ValidationSuccessfullPage

--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import InputField from '../components/input.jsx';
+import InputField from '../components/Input.jsx';
 import NextButton from "../components/NextButton.jsx"
 import { Link } from 'react-router-dom';
 
 const Password = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h2>Password Page</h2>
-      <InputField />
+      <h1>Skriv inn passord</h1>
+      <InputField placeholder={"Passord"}/>
       <NextButton>
       <Link to="/authentication">Neste</Link>
       </NextButton>
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NextButton from "../components/NextButton.jsx"
 import "../index.css"
+import InputField from '../components/Input.jsx';
 const Homepage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Hello</h1>
-      <input type="text" />
+      <h1>Skriv inn fødselsnummer</h1>
+      <InputField  placeholder={"11 tall"} />
       <div>
         <NextButton>
           <Link to="/password">Neste</Link>
