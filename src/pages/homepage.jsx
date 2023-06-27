702677e1d4e0b84d889104c4f5b947ef6f9ce29a
Ugulen <eiril.solveig.ugulen@digdir.no>
2023-06-27 08:24:11 +0200

    theme buttons are now on Homepage and when pressed change the theme of the whole Homepage

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import NextButton from "../components/NextButton.jsx"
 import "../index.css"
+import StyledMinId from '../components/StyledMinId.jsx';
+import { useState } from 'react';
+import { themes } from '../themes.js';
+import InputField from '../components/input.jsx';
+
 const Homepage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>Hello</h1>
-      <input type="text" />
+  const [theme, setTheme] = useState("theme2");
+
+  const changeTheme = (selectedTheme) => {
+    setTheme(selectedTheme);
+  };
+  
+  return ( 
+    <div className={themes[theme].background}>
+      <StyledMinId changeTheme={changeTheme}/>
       <div>
-        <NextButton>
-          <Link to="/password">Neste</Link>
-        </NextButton>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1>Hello</h1>
+          <InputField />
+          <NextButton>
+            <Link to="/password">Neste</Link>
+          </NextButton>
+        </div>
       </div>
-    </div>
+    </div> 
+    
   );
 };
 
 export default Homepage;
+
