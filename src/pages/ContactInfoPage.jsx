9cdfab48730b53262765314e55d0cf9a5df51300
AudunOklevik <audun.oklevik@gmail.com>
2023-06-28 14:17:49 +0200

    Added endre buttons

--- src/pages/ContactInfoPage.jsx
+++ src/pages/ContactInfoPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import InputField from '../components/InputField.jsx';
 import NextButton from "../components/NextButton.jsx"
 import { Link } from 'react-router-dom';
 import BackButton from '../components/BackButton.jsx';
 import ProgressBar from '../components/ProgressBar.jsx';
+import UpdateContactInfoField from '../components/UpdateContactInfoField.jsx';
 
 const ContactInfoPage = () => {
   return (
@@ -11,9 +11,9 @@ const ContactInfoPage = () => {
       <ProgressBar totalTasks={3} completedTasks={3} /> 
       <h1 className='py-5'>Se etter om kontaktinformasjonen stemmer</h1>
       <h1>Telefonnummer</h1>
-      <InputField className='color-black' placeholder="+4712345678" pattern="[0-9]+" />
+      <UpdateContactInfoField className='color-black' placeholder="+4712345678" pattern="[0-9]+" />
       <h1>Epost</h1>
-      <InputField className='color-black' placeholder="ola.nordmann@gmail.com" />
+      <UpdateContactInfoField className='color-black' placeholder="ola.nordmann@gmail.com" />
       <NextButton>
       <Link to="/validationSuccessfull">Neste</Link>
       </NextButton>
