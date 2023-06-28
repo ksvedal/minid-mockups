3f7031752196a7c9d47ff451b2e742abe201291d
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2023-06-28 14:55:54 +0200

    Changes to AuthenticationPage and ContactInfoPage (#45)
    * Completed isssue and added phone photo
    
    * Added 1 small input box, adjusted style and added phone image
    
    * Added progressbars
    
    * Added endre buttons
    
    * Fixed merge conflict
    
    * Removed excess image tag
    
    * Changed color of progressbar

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const ProgressBar = ({ totalTasks, completedTasks }) => {
+
+  return (
+    <div className="flex items-center justify-center space-x-0">
+      {[...Array(totalTasks)].map((_, index) => {
+        const isCompleted = index < completedTasks;
+        const isFirstCompleted = index === 0;
+        const showLine = !isFirstCompleted 
+
+        return (
+          <React.Fragment key={index}>
+            {showLine && (
+              <div className={`h-1.5 w-6 ${isCompleted ? 'bg-emerald-600' : 'bg-gray-300'}`} />
+            )}
+            <div
+              className={`h-6 w-6 rounded-full ${isCompleted ? 'bg-emerald-600' : 'bg-gray-300'}`}
+            />
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/pages/ContactInfoPage.jsx
+++ src/pages/ContactInfoPage.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import InputField from '../components/InputField.jsx';
 import NextButton from "../components/NextButton.jsx"
 import { Link } from 'react-router-dom';
 import BackButton from '../components/BackButton.jsx';
+import ProgressBar from '../components/ProgressBar.jsx';
+import UpdateContactInfoField from '../components/UpdateContactInfoField.jsx';
 
 const ContactInfoPage = () => {
   return (
     <div>
-      <h1>Skriv inn telefonnummer</h1>
-      <InputField placeholder="8 tall" pattern="[0-9]+" />
-      <h1>Skriv inn epost</h1>
-      <InputField placeholder="epostadresse" />
+      <ProgressBar totalTasks={3} completedTasks={3} /> 
+      <h1 className='py-5'>Se etter om kontaktinformasjonen stemmer</h1>
+      <h1>Telefonnummer</h1>
+      <UpdateContactInfoField className='color-black' placeholder="+4712345678" pattern="[0-9]+" />
+      <h1>Epost</h1>
+      <UpdateContactInfoField className='color-black' placeholder="ola.nordmann@gmail.com" />
       <NextButton>
       <Link to="/validationSuccessfull">Neste</Link>
       </NextButton>
