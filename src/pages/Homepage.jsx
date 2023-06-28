2328e249aa639ec2fa4c334e9b3591555feb92d8
Eiril <136449799+Eiril@users.noreply.github.com>
2023-06-28 16:32:18 +0200

    39 implement error feedback (#47)
    * error message appears on Homepage and you are not allowed to click next if you don't type 11 numbers
    
    * removed use of InputField component from Homepage, now error functionality works correctly
    
    * removed console log and whitespace (:
    
    ---------
    
    Co-authored-by: Eiril Ugulen <eiril.solveig.ugulen@digdir.no>

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import NextButton from "../components/NextButton.jsx"
 import "../index.css"
-import InputField from '../components/InputField.jsx';
 import BackButton from '../components/BackButton.jsx';
 import ProgressBar from '../components/ProgressBar.jsx';
+import { useState } from 'react';
 
 const Homepage = () => {
+  const navigate = useNavigate(); 
+
+  const [input, setInput] = useState("");
+  const[errormsg, setErrormsg] = useState("");
+
+  const handleInput = (event) => {
+    setInput(event.target.value); 
+  };
+
+  const handleNextButtonClick = () => {
+    if (input.length === 11){
+      setErrormsg('');
+      navigate("/password"); 
+    } 
+    else {
+      setErrormsg('Fødselsnummer må inneholde 11 siffer');
+    }
+  }
+
   return (
     <div>
       <ProgressBar totalTasks={3} completedTasks={1} />
       <h1>Skriv inn fødselsnummer</h1>
-      <InputField  placeholder={"11 tall"} pattern={"[0-9]+"} />
+      <input className={"p-3 m-1 bg-white border rounded-full w-full font-semibold"} 
+      placeholder={"11 tall"} pattern={"[0-9]+"} value={input} onChange={handleInput}>
+      </input>  
       <div>
-        <NextButton>
-          <Link to="/password">Neste</Link>
+        <NextButton onClick={handleNextButtonClick}>
+          Neste
         </NextButton>
         <BackButton>
           Tilbake
         </BackButton>
       </div>
+      {errormsg && <p>{errormsg}</p>}
     </div>
   );
 };
