0aee8bbdddf843d1c7510d1a3a9c5a9623ca6f64
AudunOklevik <audun.oklevik@gmail.com>
2023-06-29 13:17:38 +0200

    Fixed mobile phone placement

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -11,23 +11,29 @@ const AuthenticationPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <ProgressBar totalTasks={3} completedTasks={3} />
-        <div className={"absolute m-10 w-full px-10 bottom-0"}>
-        <img className='py-3 w-full' src={PhoneImage} width="70px" alt="Grey Smartphone" />
-
-      <h1 className={"text-center"}>Skriv inn engangskode fra sms</h1>
-      <div className="flex items-center justify-center space-x-2 m-2">
-        <SmallInputField placeholder={"0"} pattern={"[0-9]"} />
-        <SmallInputField placeholder={"0"} pattern={"[0-9]"} />
-        <SmallInputField placeholder={"0"} pattern={"[0-9]"} />
-        <SmallInputField placeholder={"0"} pattern={"[0-9]"} />
-        <SmallInputField placeholder={"0"} pattern={"[0-9]"} />
-      </div>
-
-            <Link to="/contactInfo"> <NextButton /> </Link>
-            <Link to="/password"> <BackButton /> </Link>
+      <div className="w-full px-10 flex flex-col items-center">
+        <div className="flex items-center justify-center">
+          <img className="pb-5" src={PhoneImage} width="100px" alt="Grey Smartphone" />
         </div>
+        <h1 className="text-center">Skriv inn engangskode fra sms</h1>
+        <div className="mb-14 flex items-center justify-center space-x-2 m-2">
+          <SmallInputField placeholder="0" pattern="[0-9]" />
+          <SmallInputField placeholder="0" pattern="[0-9]" />
+          <SmallInputField placeholder="0" pattern="[0-9]" />
+          <SmallInputField placeholder="0" pattern="[0-9]" />
+          <SmallInputField placeholder="0" pattern="[0-9]" />
+        </div>
+
+        <div className={"absolute m-10 w-full px-10 bottom-0"}>
+          <Link to="/contactInfo">
+            <NextButton />
+          </Link>
+          <Link to="/password">
+            <BackButton />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
-
 export default AuthenticationPage;
