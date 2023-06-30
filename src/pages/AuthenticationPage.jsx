4a31e4b7732cc7af32c030c28756864b773dbd78
Eiril Ugulen <eiril.solveig.ugulen@digdir.no>
2023-06-30 15:19:37 +0200

    added functionality for error message on Authentication page

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import {Link, useNavigate} from 'react-router-dom';
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 import NavigationButton from "../components/NavigationButton.jsx"
 import "../index.css"
-import SmallInputField from '../components/SmallInputField.jsx';
 import ProgressBar from '../components/ProgressBar.jsx';
 import PhoneImage from "../images/grey_smartphone.jpg";
 
 const AuthenticationPage = ({linkFrom, linkTo, completedTasks, totalTasks}) => {
     const navigate = useNavigate();
+    const [inputValues, setInputValues] = useState(["","","","",""]);
+    const [errorMessage, setErrorMessage] = useState('');
 
+    const handleInputChange = (index, value) => {
+      const newInputValues = [...inputValues];
+      newInputValues[index] = value;
+      setInputValues(newInputValues);
+      setErrorMessage("");
+    };
+
+    const handleNextClick = () => {
+      const isAnyFieldEmpty = inputValues.some((value) => value === "");
+      if (isAnyFieldEmpty) {
+        setErrorMessage("Feil kode inntastet");
+        return; 
+      }
+      navigate(linkTo);
+    };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
@@ -19,18 +35,23 @@ const AuthenticationPage = ({linkFrom, linkTo, completedTasks, totalTasks}) => {
         </div>
         <h1 className="text-center">Skriv inn engangskode fra sms</h1>
         <div className="mb-14 mt-2 flex items-center justify-center space-x-2">
-          <SmallInputField placeholder="0" pattern="[0-9]" />
-          <SmallInputField placeholder="0" pattern="[0-9]" />
-          <SmallInputField placeholder="0" pattern="[0-9]" />
-          <SmallInputField placeholder="0" pattern="[0-9]" />
-          <SmallInputField placeholder="0" pattern="[0-9]" />
+          <input placeholder="0" pattern="[a-zA-Z0-9]" onChange={(value) => handleInputChange(0, value)} 
+          className={"py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"}/>
+          <input placeholder="0" pattern="[a-zA-Z0-9]" onChange={(value) => handleInputChange(1, value)} 
+          className={"py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"}/>
+          <input placeholder="0" pattern="[a-zA-Z0-9]" onChange={(value) => handleInputChange(2, value)}
+          className={"py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"} />
+          <input placeholder="0" pattern="[a-zA-Z0-9]" onChange={(value) => handleInputChange(3, value)}
+          className={"py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"} />
+          <input placeholder="0" pattern="[a-zA-Z0-9]" onChange={(value) => handleInputChange(4, value)}
+          className={"py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"} />
         </div>
+        {errorMessage && <p className="text-red-700">{errorMessage}</p>}
       </div>
       <div className={"absolute m-10 w-1/2 px-10 bottom-0"}>
-
-          <NavigationButton onClick={() => navigate(linkTo)}/>
-          <NavigationButton text={"Avbryt"} onClick={() => navigate(linkFrom)}/>
-        </div>
+        <NavigationButton onClick={handleNextClick}/>
+        <NavigationButton text={"Avbryt"} onClick={() => navigate(linkFrom)}/>
+      </div>
     </div>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
   const regex = /^[0-9]+$/;
   const navigate = useNavigate();
 
-  const [input, setInput] = useState();
+  const [input, setInput] = useState("");
   const[errormsg, setErrormsg] = useState("");
 
   const handleInput = (event) => {
@@ -18,14 +18,14 @@ const Homepage = () => {
   };
 
   const handleNextButtonClick = () => {
-    if (input && input.length === 11 && regex.test(input)) {
+    if (input.length === 11 && regex.test(input)) {
       navigate("/password");
     } else if (input.length === 1) {
       navigate("/activationLetter");
     } else if (input.length === 2) {
       navigate("/noActivationLetter");
     } else {
-      setErrormsg('Fødselsnummer må inneholde 11 siffer og kun tall');
+      setErrormsg('Fødselsnummer må inneholde 11 siffer');
     }
   };
 
@@ -34,7 +34,7 @@ const Homepage = () => {
         <ProgressBar totalTasks={3} completedTasks={1} />
         <h1>Skriv inn fødselsnummer</h1>
         <input maxLength={11} className={"p-3 bg-custom-light-grey text-center text-black placeholder-custom-dark-grey my-2 rounded-full w-auto font-semibold dark:bg-custom-very-darker-grey dark:text-white"}
-               placeholder={"11 tall"} pattern={"[0-9]+"} value={input} onChange={handleInput}>
+               placeholder={"11 tall"} pattern={"[0-9]"} value={input} onChange={handleInput}>
         </input>
         <p className={"my-2 text-red-700" }> {errormsg && <p>{errormsg}</p>}</p>
         <div className={"absolute m-8 w-1/2 px-10 bottom-0 text-center"}>
