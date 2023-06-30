e3b79bd63c23ecfbfcf45490a7aec7efe0a432b6
Eiril Ugulen <eiril.solveig.ugulen@digdir.no>
2023-06-30 09:55:32 +0200

    added darkmode style for inputfield in passwordPage

--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -28,9 +28,9 @@ const Password = () => {
       <div className={"flex flex-col items-center justify-center h-screen"}>
         <ProgressBar totalTasks={3} completedTasks={2} />
       </div>
-          <div className={"flex flex-col items-center justify-center h-screen"}>
+          <div className={"flex flex-col items-center justify-center h-screen mb-20"}>
           <h1>Skriv inn passord</h1>
-          <input className={"text-black p-3 m-1 bg-white border rounded-full w-auto font-semibold"}
+          <input className={"text-black p-3 m-1 bg-white border rounded-full w-auto font-semibold dark:bg-custom-very-darker-grey dark:text-white"}
           placeholder={"Passord"} type={"password"} value={input} onChange={handleInput}>
           </input>
           <p className={"my-2 text-red-700"}>{errormessage && <p> {errormessage}</p >}</p>
