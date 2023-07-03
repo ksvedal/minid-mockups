97c414c373c943562755e40679b744e32b92738e
BrageNavarsete <72668683+BrageAsperandenNavarsete@users.noreply.github.com>
2023-07-03 08:56:11 +0200

    fix stay in place when error is made (#80)
    Co-authored-by: Brage Navarsete <brage2000@hotmail.com>

--- src/pages/PasswordLoginPage.jsx
+++ src/pages/PasswordLoginPage.jsx
@@ -13,36 +13,35 @@ const PasswordLoginPage = () => {
   };
 
   const handleClick = () => {
-    if (input.length !== 0){
+    if (input.length !== 0) {
       setErrormessage("");
       navigate("/authentication1");
-    }
-    else {
+    } else {
       setErrormessage("Feil passord inntastet");
     }
   }
 
   return (
-      <>
+    <>
       <div className={"flex flex-col items-center justify-center h-screen"}>
         <ProgressBar totalTasks={3} completedTasks={2} />
       </div>
-          <div className={"flex flex-col items-center justify-center h-screen mb-20"}>
-          <h1>Skriv inn passord</h1>
-              <input className={"text-black bg-custom-light-grey p-3 m-1 dark:bg-custom-very-darker-grey dark:text-white text-center rounded-full w-auto font-semibold"}
-                     placeholder={"Passord"} type={"password"} value={input} onChange={handleInput}>
-              </input>
-          <p className={"my-2 text-red-700"}>{errormessage && <p> {errormessage}</p >}</p>
-      <Link to="/authentication2">
+      <div className={"flex flex-col items-center justify-center h-screen mb-20"}>
+        <h1>Skriv inn passord</h1>
+        <input className={`text-black bg-custom-light-grey p-3 m-1 dark:bg-custom-very-darker-grey text-center rounded-full w-auto font-semibold`}
+          placeholder={"Passord"} type={"password"} value={input} onChange={handleInput} />        
+          <p className={"my-2 text-red-700 absolute mt-14"}>{errormessage && <p> {errormessage}</p >}</p>
+        <Link to="/authentication2">
           <h2 className='mt-10'>Glemt passord</h2>
-      </Link>
+        </Link>
       </div>
-    <div className={"absolute w-1/2 px-10 bottom-0 m-10"}>
+      <div className={"absolute w-1/2 px-10 bottom-0 m-10"}>
         <NavigationButton onClick={handleClick} />
         <NavigationButton text={"Tilbake"} onClick={() => navigate("/.")} />
-    </div>
-      </>
+      </div>
+    </>
   );
 };
 
 export default PasswordLoginPage;
+
