f61ee56d5a663e5ec7c678ea7cd6f045ea8093e4
AudunKristian <123725511+AudunKristian@users.noreply.github.com>
2023-07-03 12:52:40 +0200

    41 make the cursor move to next circle automaticly when a digit has been typed in authentication page (#85)
    * Completed isssue and added phone photo
    
    * Added 1 small input box, adjusted style and added phone image
    
    * Added progressbars
    
    * Added endre buttons
    
    * Fixed merge conflict
    
    * Removed excess image tag
    
    * Changed color of progressbar
    
    * Issue completed
    
    * Backwards functionality fixed
    
    * Soon finished
    
    * Merge v2
    
    * Yay! merged
    
    * Merge again
    
    * Language changes
    
    * Ok merge

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -40,6 +40,14 @@ const AuthenticationPage = ({linkFrom, linkTo, completedTasks, totalTasks}) => {
     navigate(linkTo);
   };
 
+  const handleKeyDown = (index, event) => {
+    if (event.key === 'Backspace' && event.target.value === '') {
+      if (index > 0) {
+        inputRefs.current[index - 1].focus(); // Move focus to the previous input
+      }
+    }
+  };
+   
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <ProgressBar totalTasks={totalTasks} completedTasks={completedTasks} />
@@ -52,42 +60,47 @@ const AuthenticationPage = ({linkFrom, linkTo, completedTasks, totalTasks}) => {
           <input
             ref={(ref) => inputRefs.current[0] = ref} // Store the input reference in inputRefs.current[0]
             placeholder="0"
-            pattern="[0-9]" // Accept digits only
+            pattern="[a-zA-Z0-9]" 
             maxLength={1}
             onChange={(event) => handleInputChange(0, event)}
+            onKeyDown={(event) => handleKeyDown(0, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           {/* Repeat the above pattern for the remaining inputs (1, 2, 3, 4) */}
           <input
             ref={(ref) => inputRefs.current[1] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(1, event)}
+            onKeyDown={(event) => handleKeyDown(1, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
             ref={(ref) => inputRefs.current[2] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(2, event)}
+            onKeyDown={(event) => handleKeyDown(2, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
             ref={(ref) => inputRefs.current[3] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(3, event)}
+            onKeyDown={(event) => handleKeyDown(3, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
             ref={(ref) => inputRefs.current[4] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(4, event)}
+            onKeyDown={(event) => handleKeyDown(4, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
         </div>
--- src/pages/forgotPassword/EmailValidationPage.jsx
+++ src/pages/forgotPassword/EmailValidationPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
 import '../../index.css';
 import ProgressBar from '../../components/ProgressBar.jsx';
 import MailImage from '../../images/envelope.png';
@@ -7,8 +7,11 @@ import NavigationButton from '../../components/NavigationButton';
 import {useTranslation} from "react-i18next";
 
 const EmailValidationPage = () => {
-    const {t} = useTranslation()
-  const [inputValues, setInputValues] = useState(['', '', '', '', '', '']);
+  const {t} = useTranslation();
+  const navigate = useNavigate();
+  const [inputValues, setInputValues] = useState(['', '', '', '', '']);
+  const [errorMessage, setErrorMessage] = useState('');
+
   const inputRefs = useRef([]);
 
   const handleInputChange = (index, event) => {
@@ -21,12 +24,30 @@ const EmailValidationPage = () => {
     const newInputValues = [...inputValues];
     newInputValues[index] = event.target.value;
     setInputValues(newInputValues);
+    setErrorMessage('');
 
     if (inputValue.length === 1 && index < inputRefs.current.length - 1) {
       inputRefs.current[index + 1].focus(); // Move focus to the next input
     }
   };
 
+  const handleNextClick = () => {
+    const isAnyFieldEmpty = inputValues.some((value) => value === '');
+    if (isAnyFieldEmpty) {
+      setErrorMessage('Feil kode inntastet');
+      return;
+    }
+    navigate("/resetPassword");
+  };
+
+  const handleKeyDown = (index, event) => {
+    if (event.key === 'Backspace' && event.target.value === '') {
+      if (index > 0) {
+        inputRefs.current[index - 1].focus(); // Move focus to the previous input
+      }
+    }
+  };
+   
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <ProgressBar totalTasks={5} completedTasks={4} />
@@ -36,64 +57,58 @@ const EmailValidationPage = () => {
         </div>
         <h1 className="text-center">{t('enterActivationCodeByEmail')}</h1>
         <div className="mb-3 mt-2 flex items-center justify-center space-x-2">
-          <input
-            ref={(ref) => (inputRefs.current[0] = ref)}
+        <input
+            ref={(ref) => inputRefs.current[0] = ref} // Store the input reference in inputRefs.current[0]
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]" 
             maxLength={1}
             onChange={(event) => handleInputChange(0, event)}
+            onKeyDown={(event) => handleKeyDown(0, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
-          {/* Repeat the above pattern for the remaining inputs (1, 2, 3, 4, 5) */}
+          {/* Repeat the above pattern for the remaining inputs (1, 2, 3, 4) */}
           <input
-            ref={(ref) => (inputRefs.current[1] = ref)}
+            ref={(ref) => inputRefs.current[1] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(1, event)}
+            onKeyDown={(event) => handleKeyDown(1, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
-            ref={(ref) => (inputRefs.current[2] = ref)}
+            ref={(ref) => inputRefs.current[2] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(2, event)}
+            onKeyDown={(event) => handleKeyDown(2, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
-            ref={(ref) => (inputRefs.current[3] = ref)}
+            ref={(ref) => inputRefs.current[3] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(3, event)}
+            onKeyDown={(event) => handleKeyDown(3, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
           <input
-            ref={(ref) => (inputRefs.current[4] = ref)}
+            ref={(ref) => inputRefs.current[4] = ref}
             placeholder="0"
-            pattern="[0-9]"
+            pattern="[a-zA-Z0-9]"
             maxLength={1}
             onChange={(event) => handleInputChange(4, event)}
-            className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
-          />
-          <input
-            ref={(ref) => (inputRefs.current[5] = ref)}
-            placeholder="0"
-            pattern="[0-9]"
-            maxLength={1}
-            onChange={(event) => handleInputChange(5, event)}
+            onKeyDown={(event) => handleKeyDown(4, event)}
             className="py-6 mb-4 w-11 h-11 bg-custom-light-grey text-black rounded-full border-custom-dark-grey-grey text-center dark:bg-custom-very-darker-grey dark:text-white"
           />
         </div>
+        <p className={"my-2 text-red-700 absolute top-40"}>{errorMessage && <p> {errorMessage}</p >}</p>
       </div>
       <div className="absolute m-10 w-1/2 px-10 bottom-0">
-        <Link to="/resetPassword">
-          <NavigationButton />
-        </Link>
-        <Link to="/.">
-          <NavigationButton text={t('cancel')} />
-        </Link>
+        <NavigationButton onClick={handleNextClick} />
+        <NavigationButton text={t('cancel')} onClick={() => navigate("/authentication2")} />
       </div>
     </div>
   );
