f620ee3f34f07c467e3efb06e2aad23ab1b6b849
BrageNavarsete <72668683+BrageAsperandenNavarsete@users.noreply.github.com>
2023-07-05 10:36:58 +0200

    fix mountain component, dont want ocean anyways.. (#110)
    Co-authored-by: Brage Navarsete <brage2000@hotmail.com>

---
+++ implementation2/src/components/Icons/ocean.jsx
@@ -0,0 +1,16 @@
+import { faWater } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+
+
+const Ocean = () => {
+
+  return (
+    <div>
+        <FontAwesomeIcon icon={faWater}/>
+    </div>
+  );
+};
+
+export default Ocean;
