54bd765d33cec5fbcfa12244725b55d790fcfd64
AudunOklevik <audun.oklevik@gmail.com>
2023-07-07 10:48:07 +0200

    Button-problem on noActivationLetterPage

--- implementation3/src/pages/ContactInfoPage.jsx
+++ implementation3/src/pages/ContactInfoPage.jsx
@@ -7,27 +7,26 @@ import UpdateContactInfoField from '../components/UpdateContactInfoField.jsx';
 import {useTranslation} from "react-i18next";
 
 const ContactInfoPage = ({linkTo, totalTasks}) => {
-    const {t} = useTranslation()
-    const navigate = useNavigate();
+  const {t} = useTranslation()
+  const navigate = useNavigate();
 
-    return (
+  return (
     <div className={"flex flex-col items-center justify-center h-screen text-custom-black dark:text-custom-white"}>
       {/*ContactInfoPage is always the last step completedTasks = totalTasks*/}
       <ProgressBar totalTasks={totalTasks} completedTasks={totalTasks} />
-      
-      <div className={"absolute top-52 left-1/2 transform -translate-x-1/2 text-3xl font-bold text-center"}>
-        <h1 className='py-5'>{t('confirmContactInformation')}</h1>
+      <div>
+        <h1 className='absolute top-52 left-1/2 transform -translate-x-1/2 text-3xl font-bold text-center'>{t('confirmContactInformation')}</h1>
         <h1>{t('phoneNumber')}</h1>
-        <UpdateContactInfoField className='color-black' placeholder="+4712345678" pattern="[0-9]+" />
+        <UpdateContactInfoField className='text-left' placeholder="+4712345678" pattern="[0-9]+" />
         <h1>{t('email')}</h1>
-        <UpdateContactInfoField className='color-black' placeholder="ola.nordmann@gmail.com" />
+        <UpdateContactInfoField className='text-left' placeholder="ola.nordmann@gmail.com" />
       </div>
 
       <Mountains />
-    
+      
       <NavigationButton position={'right'} text={t('confirm')} onClick={() => navigate(linkTo)}/>
       <NavigationButton position={'left'} text={t('back')} onClick={() => navigate("/.")}/>
-  
+
     </div>
   );
 };
--- implementation3/src/pages/activationLetter/doneRegisterUserPage.jsx
+++ implementation3/src/pages/activationLetter/doneRegisterUserPage.jsx
@@ -10,14 +10,14 @@ const DoneRegisterPage = () => {
     const navigate = useNavigate();
     return (
         <div className={"flex flex-col items-center justify-center text-custom-black dark:text-custom-white"}>
-        <ProgressBar totalTasks={4} completedTasks={4} />
-        <p className="absolute top-52 text-3xl font-bold">{t('welcomeAsMinIdUser')}</p>
-        <p className="p-32 text-center">{t('userRegistered')}</p>
+            <ProgressBar totalTasks={4} completedTasks={4} />
+            <p className="absolute top-52 text-3xl font-bold">{t('welcomeAsMinIdUser')}</p>
+            <p className="p-32 text-center">{t('userRegistered')}</p>
 
-        <Mountains />
+            <Mountains />
+            
+            <NavigationButton position={'center'} text={t('home')} onClick={() => navigate("/.")}/>
         
-        <NavigationButton position={'center'} text={t('home')} onClick={() => navigate("/.")}/>
-      
        
         </div>
     )
