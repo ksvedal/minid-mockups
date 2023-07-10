451e24d90182c173f3fa5e2dd6b5827e33a99457
BrageNavarsete <72668683+BrageAsperandenNavarsete@users.noreply.github.com>
2023-07-10 10:39:44 +0200

    now redmode should be all red (#131)
    Co-authored-by: Brage Navarsete <brage2000@hotmail.com>

--- implementation2/src/components/Icons/forest.jsx
+++ implementation2/src/components/Icons/forest.jsx
@@ -8,10 +8,10 @@ const Forest = () => {
 
   return (
     <div className={"-bottom-1 -right-4 absolute"}>
-        <FontAwesomeIcon icon={faTree} className=' absolute bottom-0 right-16 text-custom-purple text-[7rem]'/>
-        <FontAwesomeIcon icon={faTree} className=' absolute right-36 text-custom-light-purple text-[5rem]'/>
-        <FontAwesomeIcon icon={faTree} className=' absolute bottom-0 right-52 text-custom-purple text-[7rem]'/>
-        <FontAwesomeIcon icon={faTree} className=' text-custom-light-purple text-[5rem]'/>
+        <FontAwesomeIcon icon={faTree} className=' absolute bottom-0 right-16 text-custom-purple text-[7rem] dark:text-custom-red'/>
+        <FontAwesomeIcon icon={faTree} className=' absolute right-36 text-custom-light-purple text-[5rem] dark:text-custom-light-red'/>
+        <FontAwesomeIcon icon={faTree} className=' absolute bottom-0 right-52 text-custom-purple text-[7rem] dark:text-custom-red'/>
+        <FontAwesomeIcon icon={faTree} className=' text-custom-light-purple text-[5rem] dark:text-custom-light-red'/>
     </div>
   )
 };
--- implementation2/src/components/updateContactInfoField.jsx
+++ implementation2/src/components/updateContactInfoField.jsx
@@ -29,7 +29,7 @@ const UpdateContactInfoField = ({ placeholder, pattern }) => {
   return (
     <div className="flex items-center">
       <input
-        className="p-3 bg-custom-light-grey text-black placeholder-custom-dark-grey m-1 px-10 rounded-full w-full font-semibold dark:bg-custom-very-darker-grey dark:text-white"
+        className="p-3 bg-custom-light-grey text-black placeholder-custom-dark-grey m-1 px-10 rounded-full w-full font-semibold "
         type="text"
         placeholder={isEditing ? '' : placeholder}
         value={value}
@@ -37,14 +37,14 @@ const UpdateContactInfoField = ({ placeholder, pattern }) => {
         readOnly={!isEditing}
       />
       {!isEditing && (
-        <button className="p-3 my-2 ml-1 dark:bg-custom-yellow dark:text-black border-1 bg-custom-blue rounded-full
+        <button className="p-3 my-2 ml-1 dark:text-black border-1 bg-custom-blue rounded-full
         text-white font-semibold"
                 onClick={handleEdit}>
           {t('change')}
         </button>
       )}
       {isEditing && (
-        <button className="p-3 my-2 ml-1 dark:bg-custom-yellow dark:text-black border-1 bg-custom-blue rounded-full
+        <button className="p-3 my-2 ml-1  dark:text-black border-1 bg-custom-blue rounded-full
         text-white font-semibold"
                 onClick={handleSave}>
           {t('save')}
